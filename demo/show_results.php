9f3957ede06d49819868b5e69f0e187d3bdb3175
BenjamimCS <83431838+BenjamimCS@users.noreply.github.com>
2021-12-16 19:52:15 -0300

    Uploading Demo Page

---
+++ demo/show_results.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Topics</title>
+    <link rel="stylesheet" href="../decoration/style.css">
+    <style>
+
+</style>
+</head>
+<body>
+    <main>
+        <h1>Topics</h1> 
+        <section id="results">
+            <?php
+            $baseDir = './';
+            $openDir = dir($baseDir);
+            
+            while($arq = $openDir->read()){
+                if($arq !== '.' && $arq !== '..' && !strpos($arq,'.php') && !is_dir($arq)){
+                    $content = file_get_contents($arq);
+            
+                    $dom = new DOMDocument();
+                    libxml_use_internal_errors(true);
+                    $dom->validateOnParse = true;
+                    $dom->loadHTML($content);
+            
+                    $h1 = $dom->getElementById('titulo')->textContent;
+                    $h2 = $dom->getElementById('subtitulo')->textContent;
+                    $p1 = rtrim(substr($dom->getElementById('lead')->textContent,0,90))."...";
+            
+                    echo
+                    "
+                    <a href='".$arq."' rel='next'>
+                        <div class='result-area'>
+                            <h1>".$h1."</h1>
+                            <h2>".$h2."</h1>
+                            <p>".$p1."</p>
+                        </div>
+                    </a>
+                    ";
+                }
+            }
+            ?>
+        </section>
+    </main>
+</body>
