0f8803614d5bccd52e1b7412ab35c9596f6827b3
BenjamimCS <83431838+BenjamimCS@users.noreply.github.com>
2021-12-26 23:00:18 -0300

    Unnecessaray Style Tag Removed

--- demo/show_results.php
+++ demo/show_results.php
@@ -6,9 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Topics</title>
     <link rel="stylesheet" href="./decoration/style.css">
-    <style>
-
-</style>
 </head>
 <body>
     <main>
